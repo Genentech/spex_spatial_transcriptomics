b4cc76a4178b6f0b9e16c0a17df216a268898094
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-02-05 11:23:54 +0700

    small fixes

--- load_anndata/app.py
+++ load_anndata/app.py
@@ -1,8 +1,15 @@
 import anndata
+import numpy as np
 
 
 def run(**kwargs):
 
     adata = anndata.read(kwargs.get('image_path'))
+    try:
+        adata.obsm['spatial'] = np.stack([adata.obs.x_centroid, adata.obs.y_centroid]).T
+    except AttributeError:
+        print('No x_centroid and y_centroid found.')
+    finally:
+        adata.obsm['spatial'] = adata.obsm['spatial'].astype('float')
 
     return {'adata': adata}
