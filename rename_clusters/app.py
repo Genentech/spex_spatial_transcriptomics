ec0dbb104736c7e0c7e290344f4e2e3a3221871e
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-05-22 12:48:00 +0700

    fixed cluster rename

--- rename_clusters/app.py
+++ rename_clusters/app.py
@@ -1,18 +1,18 @@
 from anndata import AnnData
 
-clusters_to_merge = [
-    {'0': 'A'}, {'1': 'A'}, {'2': 'A'}, {'19': 'B'}
-]
+# clusters_to_merge = [
+#     {'0': 'A'}, {'1': 'A'}, {'2': 'A'}, {'19': 'B'}
+# ]
 
 def run(**kwargs):
 
     adata = kwargs.get('adata')
     clust_col = adata.obs.columns[-1:][0]
-    cluster_list = kwargs.get('cluster_list')
+    cluster_list = kwargs.get('cluster_list', {})
 
     adata.obs[clust_col] = adata.obs[clust_col].astype(str)
 
-    for cluster_map in clusters_to_merge:
+    for cluster_map in cluster_list:
         for old_cluster, new_cluster in cluster_map.items():
             adata.obs[clust_col] = adata.obs[clust_col].replace(old_cluster, new_cluster)
     adata.obs[clust_col] = adata.obs[clust_col].astype('category')
