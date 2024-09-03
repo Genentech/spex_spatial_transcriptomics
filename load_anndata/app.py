435692b98cdf41456a58716d455a871ee568d80a
Aleksandr Prilipko <alexander.prilipko@gmail.com>
2024-09-04 06:25:51 +0700

    merge batch key

--- load_anndata/app.py
+++ load_anndata/app.py
@@ -5,5 +5,8 @@ def run(**kwargs):
 
     path = kwargs.get('image_path')
     adata = sc.read_h5ad(path)
+    # on merge multiple files
+    # if 'batch_key' not in adata.uns:
+    #     adata.uns['batch_key'] = 'fov'
 
     return {'adata': adata}
