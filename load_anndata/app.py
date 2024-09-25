32028a935cea7415b1de5a0da28003e236bf2c5d
Aleksandr Prilipko <alexander.prilipko@gmail.com>
2024-09-25 12:33:55 +0700

    added combine anndata

--- load_anndata/app.py
+++ load_anndata/app.py
@@ -1,12 +1,17 @@
 import scanpy as sc
 
-
 def run(**kwargs):
-
+    files = kwargs.get('files', [])
     path = kwargs.get('image_path')
-    adata = sc.read_h5ad(path)
-    # on merge multiple files
-    # if 'batch_key' not in adata.uns:
-    #     adata.uns['batch_key'] = 'fov'
 
-    return {'adata': adata}
+    if files:
+        adatas = []
+        for file in files:
+            adata = sc.read_h5ad(file)
+            adata.obs['filename'] = file.split('\\')[-1]
+            adatas.append(adata)
+        combined_adata = sc.concat(adatas, axis=0, join='outer', label='filename', index_unique='-')
+        return {'adata': combined_adata}
+    else:
+        adata = sc.read_h5ad(path)
+        return {'adata': adata}
